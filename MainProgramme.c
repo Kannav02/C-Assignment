70de2d5affab1368a20a1e5566ab581813f6aa16
Kannav Sethi <kannavsethi2@gmail.com>
2021-11-13 23:52:02 +0530

    Functions Added to the main file

--- MainProgramme.c
+++ MainProgramme.c
@@ -3,7 +3,7 @@
 
 
 #define _CRT_SECURE_NO_WARNINGS
-
+#define InterestRate 0.05
 #include <stdio.h>
 #include <string.h>
 #include "MainHeaderFile.h"
@@ -102,4 +102,120 @@ int main(void) {
 	fclose(fp);
 
 	return 0;
+}
+
+
+
+
+
+
+double MonthlyEarnings(double MonthlyRent,double MonthlyUtilities ,double MonthlyPropertyTax){
+    double monthlyEarnings;
+    monthlyEarnings = MonthlyRent-MonthlyUtilities-MonthlyPropertyTax;
+    return monthlyEarnings;
+
+}
+// calculate the Return on investment for a single property
+double ReturnOnInvestment(double MonthlyEarnings,double Pprice){
+    double rinvestment;
+    rinvestment=100*12*MonthlyEarnings/Pprice;
+    return rinvestment;
+    
+    
+}
+// calculate the current value of a single property
+double CurrentValue(double MonthlyEarnings){
+    double cValue;
+    cValue=12*MonthlyEarnings/InterestRate;
+    return cValue;
+
+}
+// calculate the capitalgains for a single property
+double CapitalGains(double CurrentValue, double Pprice){
+    double CapitalG;
+    CapitalG=CurrentValue-Pprice;
+    return CapitalG;
+}
+// calculate the total monthly earnings for a specific property
+double TotalMonthlyEarningsAppart(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->appartments[i].MonthlyEarnings;
+
+    }
+    return SumM;
+}
+
+double TotalCvalueAppart(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->appartments[i].CurrentV;
+
+    }
+    return SumM;
+}
+
+double CapitalGainsAppart(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->appartments[i].CapitalG;
+
+    }
+    return SumM;
+}
+
+
+
+double TotalMonthlyEarningsTownH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->townhouses[i].MonthlyEarnings;
+
+    }
+    return SumM;
+}
+
+double TotalCvalueTownH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->townhouses[i].CurrentV;
+
+    }
+    return SumM;
+}
+
+double CapitalGainsTownH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->townhouses[i].CapitalG;
+
+    }
+    return SumM;
+}
+
+double TotalMonthlyEarningsSemiH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->houses[i].MonthlyEarnings;
+
+    }
+    return SumM;
+}
+
+double TotalCvalueSemiH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->houses[i].CurrentV;
+
+    }
+    return SumM;
+}
+
+double CapitalGainsSemiH(struct RealEstate *property){
+    double SumM=0;
+    for(int i =0;i<NUM_PROPERTY;i++){
+        SumM=SumM+property->houses[i].CapitalG;
+
+    }
+    return SumM;
 }
